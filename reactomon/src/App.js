16a14f4cc49070dc807001e6fbbb844222a4d0c0
Lilla Kertesz <lil.kertesz@gmail.com>
2020-09-16 20:03:56 +0200

    fetching data and displaying it on pokemons route

--- reactomon/src/App.js
+++ reactomon/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import PokemonList from "./components/PokemonList";
+import TypeList from "./components/TypeList";
+// import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className="navbar navbar-light">
+          <ul className="nav navbar-nav">
+            <li>
+              <Link to="/pokemons">pokemons</Link>
+            </li>
+            <li>
+              <Link to="/types">types</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/pokemons" component={PokemonList} />
+        <Route path="/types" component={TypeList} />
+      </div>
+    </Router>
   );
 }
 
