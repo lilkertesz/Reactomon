5b8354b1cc34ec214848c198908e842da031ab7a
Lilla Kertesz <lil.kertesz@gmail.com>
2020-09-18 21:18:05 +0200

    navbar styling

--- reactomon/src/App.js
+++ reactomon/src/App.js
@@ -1,29 +1,15 @@
 import React from "react";
 // import { Link, Route, Switch } from "react-router-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
-import PokemonList from "./components/PokemonList";
-import TypeList from "./components/TypeList";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav className="navbar navbar-light">
-          <ul className="nav navbar-nav">
-            <li>
-              <Link to="/pokemons">pokemons</Link>
-            </li>
-            <li>
-              <Link to="/types">types</Link>
-            </li>
-          </ul>
-        </nav>
-        <Route path="/pokemons" component={PokemonList} />
-        <Route path="/types" component={TypeList} />
-      </div>
-    </Router>
+      <React.Fragment>
+        <Navbar></Navbar>
+      </React.Fragment>
+      
   );
 }
 
--- reactomon/src/components/Navbar.js
+++ reactomon/src/components/Navbar.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PokemonList from "./PokemonList";
+import TypeList from "./TypeList";
+import "./style.css";
 
 class Navbar extends Component {
   render() {
     return (
-      <div>
-        <p>Navbar</p>
-      </div>
+      <Router>
+        <div className="navbar">
+          <div className="card bg-warning">
+            <Link to="/pokemons">POKEMONS</Link>
+          </div>
+          <img
+            src={require("./images/logo.JPG")}
+            alt="pokelogo"
+            width="100px"
+          />
+          <div className="card bg-warning">
+            <Link to="/types">POKETYPES</Link>
+          </div>
+        </div>
+        <Route path="/pokemons" component={PokemonList} />
+        <Route path="/types" component={TypeList} />
+      </Router>
     );
   }
 }
